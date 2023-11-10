7222fb470a4c5f8a204ca2695992201fc6729868
Muhammad Ardhiansyah <ardhi.ansyah2662@gmail.com>
2023-11-10 20:54:05 +0700

    add banner for mobile and tablet

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ function App() {
     <>
       <div className="bg-[#333]">
         <div className="banner w-full  flex bg-[url('/space.jpg')] bg-no-repeat bg-cover justify-center">
-          <div className="banner__wrapper flex items-center justify-between px-10 min-h-[600px] w-full max-w-[1366px] ">
-            <div className="banner__text flex flex-col gap-2 justify-center w-1/2 h-full">
+          <div className="banner__wrapper flex flex-col-reverse md:flex-row items-center justify-between md:px-10 lg:px-20 min-h-screen md:min-h-[600px] w-full max-w-[1366px] py-6">
+            <div className="banner__text flex flex-col gap-2 justify-center w-full md:w-1/2 h-full px-4 md:px-0">
               <div className="banner__text__title  font-black text-3xl text-white drop-shadow-[0_0_10px_#1347f3]">
                 <h1>Don't Get Lost.</h1>
               </div>
@@ -27,7 +27,7 @@ function App() {
                 </button>
               </div>
             </div>
-            <div className="banner__image flex items-center justify-center w-1/2">
+            <div className="banner__image flex items-center justify-center w-2/3 md:w-1/2 px-4 py-4 md:px-6">
               <img
                 width={400}
                 className="drop-shadow-[0_0_10px_#1347f3]"
