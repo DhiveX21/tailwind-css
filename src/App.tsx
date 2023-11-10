dae83195ce501119e508478ad1ea4531826a8969
Muhammad Ardhiansyah <ardhi.ansyah2662@gmail.com>
2023-11-10 20:27:25 +0700

    desktop Banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-[#333]">
+        <div className="banner w-full  flex bg-[url('/space.jpg')] bg-no-repeat bg-cover justify-center">
+          <div className="banner__wrapper flex items-center justify-between px-10 min-h-[600px] w-full max-w-[1366px] ">
+            <div className="banner__text flex flex-col gap-2 justify-center w-1/2 h-full">
+              <div className="banner__text__title  font-black text-3xl text-white drop-shadow-[0_0_10px_#1347f3]">
+                <h1>Don't Get Lost.</h1>
+              </div>
+              <div className="banner__text__subtitle text-s_yellow font-black text-5xl drop-shadow-[0_0_10px_#1347f3]">
+                <span>Be Productive!</span>
+              </div>
+              <div className="banner__text__description text-white font-bold text-md pr-4 drop-shadow-[0_0_3px_#eee]">
+                <span>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Eligendi quibusdam est recusandae magni temporibus modi nulla
+                  blanditiis dignissimos sequi non!
+                </span>
+              </div>
+              <div className="banner__text__button text-white font-bold text-md pr-4 mt-6 flex gap-4">
+                <button className="bg-s_spaceBlue px-8 py-2 rounded-full shadow-lg">
+                  Get Started
+                </button>
+                <button className="bg-s_purple px-8 py-2 rounded-full shadow-lg">
+                  Let's Go!
+                </button>
+              </div>
+            </div>
+            <div className="banner__image flex items-center justify-center w-1/2">
+              <img
+                width={400}
+                className="drop-shadow-[0_0_10px_#1347f3]"
+                src="/chill_astronaut.png"
+                alt="Astronaut in the moon."
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        s_darkPurple: "#170312",
+        s_deepPurple: "#33032F",
+        s_purple: "#531253",
+        s_spaceBlue: "#2E31C7",
+        s_yellow: "#F3B61F",
+        s_textGray: "#333",
+      },
+    },
   },
   plugins: [],
 };
